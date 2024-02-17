2099be8d730f5a8ca35fe9cfe1fc50119c9a410d
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-02-16 23:32:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,128 +1,97 @@
-const PRE = "DELTA"
-const SUF = "MEET"
-var room_id;
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-var local_stream;
-var screenStream;
-var peer = null;
-var currentPeer = null
-var screenSharing = false
-function createRoom() {
-    console.log("Creating Room")
-    let room = document.getElementById("room-input").value;
-    if (room == " " || room == "") {
-        alert("Please enter room number")
-        return;
-    }
-    room_id = PRE + room + SUF;
-    peer = new Peer(room_id)
-    peer.on('open', (id) => {
-        console.log("Peer Connected with ID: ", id)
-        hideModal()
-        getUserMedia({ video: true, audio: true }, (stream) => {
-            local_stream = stream;
-            setLocalStream(local_stream)
-        }, (err) => {
-            console.log(err)
-        })
-        notify("Waiting for peer to join.")
-    })
-    peer.on('call', (call) => {
-        call.answer(local_stream);
-        call.on('stream', (stream) => {
-            setRemoteStream(stream)
-        })
-        currentPeer = call;
-    })
-}
 
-function setLocalStream(stream) {
+    // Definindo constantes para o prefixo e sufixo do room_id
+    const PREFIX = "DELTA";
+    const SUFFIX = "MEET";
+    let room_id; // Variável para armazenar o identificador da sala
+    let getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; // Obtendo a função getUserMedia com suporte a navegadores
+    let local_stream; // Variável para armazenar o stream local
+    let peer = null; // Objeto para gerenciar a comunicação peer-to-peer
+    let currentPeer = null; // Variável para armazenar o objeto de chamada atual
 
-    let video = document.getElementById("local-video");
-    video.srcObject = stream;
-    video.muted = true;
-    video.play();
-}
-function setRemoteStream(stream) {
-
-    let video = document.getElementById("remote-video");
-    video.srcObject = stream;
-    video.play();
-}
-
-function hideModal() {
-    document.getElementById("entry-modal").hidden = true
-}
-
-function notify(msg) {
-    let notification = document.getElementById("notification")
-    notification.innerHTML = msg
-    notification.hidden = false
-    setTimeout(() => {
-        notification.hidden = true;
-    }, 3000)
-}
-
-function joinRoom() {
-    console.log("Joining Room")
-    let room = document.getElementById("room-input").value;
-    if (room == " " || room == "") {
-        alert("Please enter room number")
-        return;
-    }
-    room_id = PRE + room + SUF;
-    hideModal()
-    peer = new Peer()
-    peer.on('open', (id) => {
-        console.log("Connected with Id: " + id)
-        getUserMedia({ video: true, audio: true }, (stream) => {
-            local_stream = stream;
-            setLocalStream(local_stream)
-            notify("Joining peer")
-            let call = peer.call(room_id, stream)
+    // Função para criar uma sala
+    function createRoom() {
+        console.log("Creating Room");
+        let room = document.getElementById("room-input").value;
+        if (!room.trim()) {
+            alert("Please enter a room number");
+            return;
+        }
+        room_id = PREFIX + room + SUFFIX;
+        peer = new Peer(room_id);
+        peer.on('open', (id) => {
+            console.log("Peer Connected with ID:", id);
+            hideModal();
+            getUserMedia({ video: true, audio: true }, (stream) => {
+                local_stream = stream;
+                setLocalStream(local_stream);
+            }, (err) => {
+                console.log(err);
+            });
+            notify("Waiting for peer to join.");
+        });
+        peer.on('call', (call) => {
+            call.answer(local_stream);
             call.on('stream', (stream) => {
                 setRemoteStream(stream);
-            })
+            });
             currentPeer = call;
-        }, (err) => {
-            console.log(err)
-        })
-
-    })
-}
-
-function startScreenShare() {
-    if (screenSharing) {
-        stopScreenSharing()
+        });
     }
-    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream) => {
-        screenStream = stream;
-        let videoTrack = screenStream.getVideoTracks()[0];
-        videoTrack.onended = () => {
-            stopScreenSharing()
-        }
-        if (peer) {
-            let sender = currentPeer.peerConnection.getSenders().find(function (s) {
-                return s.track.kind == videoTrack.kind;
-            })
-            sender.replaceTrack(videoTrack)
-            screenSharing = true
-        }
-        console.log(screenStream)
-    })
-}
 
-function stopScreenSharing() {
-    if (!screenSharing) return;
-    let videoTrack = local_stream.getVideoTracks()[0];
-    if (peer) {
-        let sender = currentPeer.peerConnection.getSenders().find(function (s) {
-            return s.track.kind == videoTrack.kind;
-        })
-        sender.replaceTrack(videoTrack)
+    // Função para definir o stream local no elemento de vídeo
+    function setLocalStream(stream) {
+        let video = document.getElementById("local-video");
+        video.srcObject = stream;
+        video.muted = true;
+        video.play();
     }
-    screenStream.getTracks().forEach(function (track) {
-        track.stop();
-    });
-    screenSharing = false
-}
+
+    // Função para definir o stream remoto no elemento de vídeo
+    function setRemoteStream(stream) {
+        let video = document.getElementById("remote-video");
+        video.srcObject = stream;
+        video.play();
+    }
+
+    // Função para ocultar o modal de entrada
+    function hideModal() {
+        document.getElementById("entry-modal").hidden = true;
+    }
+
+    // Função para exibir notificação
+    function notify(msg) {
+        let notification = document.getElementById("notification");
+        notification.innerHTML = msg;
+        notification.hidden = false;
+        setTimeout(() => {
+            notification.hidden = true;
+        }, 3000);
+    }
+
+    // Função para entrar em uma sala existente
+    function joinRoom() {
+        console.log("Joining Room");
+        let room = document.getElementById("room-input").value;
+        if (!room.trim()) {
+            alert("Please enter a room number");
+            return;
+        }
+        room_id = PREFIX + room + SUFFIX;
+        hideModal();
+        peer = new Peer();
+        peer.on('open', (id) => {
+            console.log("Connected with Id: " + id);
+            getUserMedia({ video: true, audio: true }, (stream) => {
+                local_stream = stream;
+                setLocalStream(local_stream);
+                notify("Joining peer");
+                let call = peer.call(room_id, stream);
+                call.on('stream', (stream) => {
+                    setRemoteStream(stream);
+                });
+                currentPeer = call;
+            }, (err) => {
+                console.log(err);
+            });
+        });
+    }
