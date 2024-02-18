8852f6c40e49c0d285e6547e9436839b78ded302
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-02-18 18:02:47 -0300

    Update novo.js

--- novo.js
+++ novo.js
@@ -1,13 +1,10 @@
-// Declarar a variável peer fora das funções para que seja acessível globalmente
 var peer;
 let localMediaStream =
   navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia;
 
-
 var devInfo = document.getElementById("info-messages");
-
 
 // Adicione as credenciais do servidor TURN aqui
 let turnConfig = {
@@ -20,8 +17,8 @@ let turnConfig = {
   ],
 };
 
-function toggleLog() {
-  var logDiv = document.getElementById("log");
+function toggleLog(elementId) {
+  var logDiv = document.getElementById(elementId);
   if (logDiv.style.display === "none") {
     logDiv.style.display = "block";
   } else if (logDiv.style.display === "block") {
@@ -31,24 +28,16 @@ function toggleLog() {
   }
 }
 
-
 function toggleLogfinaly() {
-  var logDiv = document.getElementById("finaly");
-  if (logDiv.style.display === "none") {
-    logDiv.style.display = "block";
-  } else if (logDiv.style.display === "block") {
-    logDiv.style.display = "none";
-  } else if (logDiv.style.display === "") {
-    logDiv.style.display = "none";
-  }
+  toggleLog("finaly");
+  toggleLog("call");
 }
 
 // Função para criar uma sala
 function createRoom() {
   const roomId = document.getElementById("room-input").value.trim();
-  toggleLog();
+  toggleLog("log");
 
-  // Certifique-se de ter o objeto mediaStream disponível
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
     .then(function (mediaStream) {
@@ -65,70 +54,65 @@ function createRoom() {
       });
 
       peer.on("call", function (call) {
-        // Armazenar a chamada para que possamos encerrá-la posteriormente se necessário
         var currentCall = call;
 
-        // Atende a chamada, fornecendo nosso mediaStream
         call.answer(localMediaStream);
         navigator.mediaDevices
           .getUserMedia({ video: true, audio: false })
           .then(function (mediaStream) {
-            toggleLogfinaly();
-            // Restante do código permanece o mesmo
             localMediaStream = mediaStream;
             var video_local = document.getElementById("local-video");
             video_local.srcObject = localMediaStream;
             video_local.play();
+            toggleLogfinaly();
           })
           .catch(function (err) {
-            devInfo.innerHTML = "Erro ao obter o stream de mídia não é possivel fazer streamer";
-      toggleLog();
+            devInfo.innerHTML =
+              "Erro ao obter o stream de mídia não é possível fazer streamer";
+            toggleLog("log");
           });
 
         call.on("stream", function (remoteStream) {
-          devInfo.innerHTML = "Stream recebido do chamador:" + remoteStream;
+          devInfo.innerHTML =
+            "Stream recebido do chamador:" + JSON.stringify(remoteStream);
           var remoteVideo = document.getElementById("remote-video");
 
-          // Define o stream de mídia no elemento de vídeo remoto
           remoteVideo.srcObject = remoteStream;
-
-          // Inicia a reprodução do vídeo remoto
           remoteVideo.play();
         });
-        peer.on('close', () => {
-          devInfo.innerHTML = "Conexão de video incerrada.";
-          var remoteVideo = document.getElementById("remote-video");
-          toggleLog();
-          // Adicione aqui a lógica que você deseja executar quando a conexão for fechada.
+
+        peer.on("close", () => {
+          devInfo.innerHTML = "Conexão de vídeo encerrada.";
+          toggleLog("log");
         });
 
-        // Adicionar um botão ou lógica para encerrar a chamada
         document
           .getElementById("end-call-button")
           .addEventListener("click", function () {
-            // Encerrar a chamada
             if (peer) {
-              // Chama 'destroy' para fechar o par e suas conexões
               peer.destroy();
-              devInfo.innerHTML = "Par destruído. Operações de limpeza concluídas.";
+              devInfo.innerHTML =
+                "Par destruído. Operações de limpeza concluídas.";
+                toggleLogfinaly();
             } else {
-              devInfo.innerHTML = "O par não está definido. Não é possível fechar.";
+              devInfo.innerHTML =
+                "O par não está definido. Não é possível fechar.";
             }
           });
       });
     })
     .catch(function (err) {
-      devInfo.innerHTML = "Erro ao obter o stream de mídia não é possivel fazer streamer";
-      toggleLog();
+      devInfo.innerHTML =
+        "Erro ao obter o stream de mídia não é possível fazer streamer";
+      toggleLog("log");
     });
 }
 
 // Função para entrar em uma sala
 function joinRoom() {
   const destPeerId = document.getElementById("room-input").value.trim();
-  toggleLog();
+  toggleLog("log");
 
-  // Certifique-se de ter o objeto mediaStream disponível
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
     .then(function (mediaStream) {
@@ -143,59 +127,53 @@ function joinRoom() {
       peer.on("open", function (id) {
         devInfo.innerHTML = "Conectado à sala com sucesso! ID do peer:" + id;
 
-        // Iniciar uma conexão com o peer da sala (destPeerId)
         var conn = peer.connect(destPeerId);
 
         conn.on("open", function () {
           devInfo.innerHTML = "Conexão estabelecida com sucesso!";
 
-          // Iniciar a chamada de vídeo
           var call = peer.call(destPeerId, localMediaStream);
 
           navigator.mediaDevices
             .getUserMedia({ video: true, audio: false })
             .then(function (mediaStream) {
-              // Restante do código permanece o mesmo
-              toggleLogfinaly();
               localMediaStream = mediaStream;
               var video_local = document.getElementById("local-video");
               video_local.srcObject = localMediaStream;
               video_local.play();
+              toggleLogfinaly();
             })
             .catch(function (err) {
-              devInfo.innerHTML = "Erro ao obter o stream de mídia não é possivel fazer streamer";
-      toggleLog();
+              devInfo.innerHTML =
+                "Erro ao obter o stream de mídia não é possível fazer streamer";
+              toggleLog("log");
             });
 
           call.on("stream", function (remoteStream) {
-            devInfo.innerHTML = "Stream recebido do chamador:" + remoteStream;
+            devInfo.innerHTML =
+              "Stream recebido do chamador:" + JSON.stringify(remoteStream);
             var remoteVideo = document.getElementById("remote-video");
 
-            // Define o stream de mídia no elemento de vídeo remoto
             remoteVideo.srcObject = remoteStream;
-
-            // Inicia a reprodução do vídeo remoto
             remoteVideo.play();
           });
 
-          peer.on('close', () => {
-            devInfo.innerHTML = "Conexão de video incerrada.";
-            var remoteVideo = document.getElementById("remote-video");
-            toggleLog();
-            // Adicione aqui a lógica que você deseja executar quando a conexão for fechada.
+          peer.on("close", () => {
+            devInfo.innerHTML = "Conexão de vídeo encerrada.";
+            toggleLog("log");
+            toggleLogfinaly();
           });
 
-          // Adicionar um botão ou lógica para encerrar a chamada
           document
             .getElementById("end-call-button")
             .addEventListener("click", function () {
-              // Encerrar a chamada
               if (peer) {
-                // Chama 'destroy' para fechar o par e suas conexões
                 peer.destroy();
-                devInfo.innerHTML = "Par destruído. Operações de limpeza concluídas.";
+                devInfo.innerHTML =
+                  "Par destruído. Operações de limpeza concluídas.";
               } else {
-                devInfo.innerHTML = "O par não está definido. Não é possível fechar.";
+                devInfo.innerHTML =
+                  "O par não está definido. Não é possível fechar.";
               }
             });
         });
@@ -206,7 +184,8 @@ function joinRoom() {
       });
     })
     .catch(function (err) {
-      devInfo.innerHTML = "Erro ao obter o stream de mídia não é possivel fazer streamer";
-      toggleLog();
+      devInfo.innerHTML =
+        "Erro ao obter o stream de mídia não é possível fazer streamer";
+      toggleLog("log");
     });
 }
