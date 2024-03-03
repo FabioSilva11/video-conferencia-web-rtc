30fa0be06a88495bc0afbf489eed1f93e2f88e68
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-03 00:28:20 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -1,18 +1,3 @@
-function pedirPermissao() {
-  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    .then(function (stream) {
-      // Permissões concedidas, faça algo com o stream
-    })
-    .catch(function (error) {
-      console.error('Erro ao obter permissões:', error);
-      // Tentar novamente após um curto período de tempo
-      setTimeout(pedirPermissao, 2000);
-    });
-}
-
-// Chame a função para iniciar o processo de permissão
-pedirPermissao();
-
 // Adicione as credenciais do servidor TURN aqui
 let turnConfig = {
   iceServers: [
